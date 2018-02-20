85344237be5c74846b962814caf1f04bbae9a1de
Daniel Shuy <daniel_shuy@hotmail.com>
2018-02-20 17:28:26 +0800

    Add KafkaJacksonSerializer

---
+++ kafka-jackson-serializer/src/main/java/com/github/daniel/shuy/kafka/jackson/serializer/KafkaJacksonSerializer.java
@@ -0,0 +1,37 @@
+package com.github.daniel.shuy.kafka.jackson.serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+/**
+ * Serializer for Kafka that uses Jackson to marshall Objects to JSON
+ * 
+ * @param <T> Type to be serialized from.
+ */
+public class KafkaJacksonSerializer<T> implements Serializer<T> {
+    private final ObjectMapper objectMapper;
+
+    public KafkaJacksonSerializer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (JsonProcessingException | RuntimeException e) {
+            throw new SerializationException("Error serializing to JSON with Jackson", e);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
